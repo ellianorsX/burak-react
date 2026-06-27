0b843e777951528c1dcefcb2c9f2533efb311070
ellianorsX <marjonathe98@icloud.com>
2026-06-27 13:57:09 +0900

    fix: organize header and footer omponents in burak react

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,33 +2,24 @@ import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 // import { RippleBadge } from "../MaterialTheme/styled";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UsersPage } from "./screens/usersPage";
+import { HelpPage } from "./screens/helpPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-        </ul>
-      </nav>
+  const location = useLocation();
+  console.log("location:", location);
 
+  return (
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -39,11 +30,15 @@ function App() {
         <Route path="/member-page">
           <UsersPage />
         </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
         <Route path="/">
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer />
+    </>
   );
 }
 
